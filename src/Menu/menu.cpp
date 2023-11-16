ae8d16108318df7d6da3330a2ac75af8c7ed8adc
cono1 <danielayg616@gmail.com>
2023-11-15 23:14:01 -0300

    Replace some magic numbers with variables

--- src/Menu/menu.cpp
+++ src/Menu/menu.cpp
@@ -3,7 +3,7 @@
 namespace game
 {
 CurrentScreen currentScreen = MENU;
-static const int fontSize = 30;
+static const int fontSize = 20;
 static Color fontColor = RED;
 
 void printVersion();
@@ -13,9 +13,21 @@ bool isButtonClicked(Button& button);
 
 void initButtons(Button& playButton, Button& creditsButton, Button& exitButton, Button& backButton)
 {
-    playButton = { {static_cast<float>(GetScreenWidth()) / 2 - 100, static_cast<float>(GetScreenHeight()) / 2 - 50, 200, 40}, "Play", false };
-    creditsButton = { {static_cast<float>(GetScreenWidth()) / 2 - 100,  static_cast<float>(GetScreenHeight()) / 2 + 10, 200, 40}, "Credits", false };
-    exitButton = { {static_cast<float>(GetScreenWidth()) / 2 - 100,  static_cast<float>(GetScreenHeight()) / 2 + 70, 200, 40}, "Exit", false };
+    float buttonWidth = 200.f;
+    float buttonHeight = 40.f;
+    int offsetX = 100;
+    int firstButtonOffsetY = -50;
+    int secondButtonOffsetY = 10;
+    int thirdButtonOffsetY = 70;
+
+    playButton = { {static_cast<float>(width) / 2 - offsetX, static_cast<float>(height) / 2 + firstButtonOffsetY,
+                    buttonWidth, buttonHeight}, "Play", false };
+
+    creditsButton = { {static_cast<float>(width) / 2 - offsetX,  static_cast<float>(height) / 2 + secondButtonOffsetY,
+                       buttonWidth, buttonHeight}, "Credits", false };
+
+    exitButton = { {static_cast<float>(width) / 2 - offsetX,  static_cast<float>(height) / 2 + thirdButtonOffsetY,
+                    buttonWidth, buttonHeight}, "Exit", false };
 
     backButton = { { 10, 10, 40, 40 }, "<-", false };
 }
@@ -53,19 +65,21 @@ void updateBackButton(Button backButton)
 
 void printVersion()
 {
-    DrawText("v 0.2 ", width -MeasureText("v 0.2 ", fontSize),
+    DrawText("v 0.2 ", width - MeasureText("v 0.2 ", fontSize),
     height - static_cast<int>(MeasureTextEx(GetFontDefault(), "v 0.2 ", fontSize, 0).y), fontSize, fontColor);
 }
 
 void drawButton(Button button) 
 {
+    int offset = 10;
+
     DrawRectangleRec(button.bounds, BLACK);
-    DrawText(button.text.c_str(), static_cast<int>(button.bounds.x + 10), static_cast<int>(button.bounds.y + 10), 20, WHITE);
+    DrawText(button.text.c_str(), static_cast<int>(button.bounds.x + offset), static_cast<int>(button.bounds.y + offset), fontSize, WHITE);
    
     if (isMouseOverButton(button))
     {
         DrawRectangleRec(button.bounds, WHITE);
-        DrawText(button.text.c_str(), static_cast<int>(button.bounds.x + 10), static_cast<int>(button.bounds.y + 10), 20, BLACK);
+        DrawText(button.text.c_str(), static_cast<int>(button.bounds.x + offset), static_cast<int>(button.bounds.y + offset), fontSize, BLACK);
     }
 }
 
